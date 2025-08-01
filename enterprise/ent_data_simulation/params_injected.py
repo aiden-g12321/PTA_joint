185599ea2024ddaf94727b2b9e52d03a602a0125
aiden-g12321 <aidengundersen36@gmail.com>
2025-08-01 11:06:59 -0600

    "building enterprise"

--- enterprise/ent_data_simulation/params_injected.py
+++ enterprise/ent_data_simulation/params_injected.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 # number of pulsars
-num_psrs = 5
+num_psrs = 1
 
 
 # number of Fourier modes to model
@@ -30,12 +30,11 @@ GWB_hypers_inj = np.array([GWB_logamp_inj, GWB_gamma_inj])
 # intrinsic pulsar red noise
 rn_model = True
 RN_seed = 334
-np.random.seed(1)
 # RN_logamps_inj = np.random.uniform(-13.5, -12.0, num_psrs)
 # RN_gammas_inj = np.random.uniform(1.0, 7.0, num_psrs)
 RN_logamps_inj = np.array([-13.2, -13.3, -13.1, -13.2, -13.1, -13.6, -13.5, -13.6, -13.8, -14.1,
                             -12.5, -13.3, -13.1, -13.2, -14.1, -12.5, -13.2, -13.1, -13.8, -14.1])[:num_psrs]
-RN_gammas_inj = np.array([5.6, 2.8, 3.5, 4.6, 3.1, 4.1, 3.8, 1.5, 5.6, 3.5,
+RN_gammas_inj = np.array([3.6, 2.8, 3.5, 4.6, 3.1, 4.1, 3.8, 1.5, 5.6, 3.5,
                           2.1, 4.8, 1.5, 2.6, 2.5, 3.1, 3.8, 1.5, 5.6, 3.5])[:num_psrs]
 # RN_logamps_inj = np.array([-12.6, -12.3, -12.3, -12.8, -12.7, -13.8, -12.8, -13.1, -13.6, -14.1,
 #                             -12.5, -13.3, -13.1, -13.2, -14.1, -12.5, -12.8, -13.1, -13.2, -14.1])[:num_psrs]
